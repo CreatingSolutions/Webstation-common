bc60aa6af6d26d53d863e336d18afbfd1996b3e9
SAUVALLE Damien <damien.sauvalle.etu@univ-lille.fr>
2019-01-21 13:06:08 +0100

    first commit

---
+++ src/main/java/webstationcommon/BackTest.java
@@ -0,0 +1,15 @@
+package webstationcommon;
+
+public class BackTest {
+
+    public String csgo;
+
+
+    public String getCsgo() {
+        return "TEst CsGo done";
+    }
+
+    public void setCsgo(String csgo) {
+        this.csgo = csgo;
+    }
+}
