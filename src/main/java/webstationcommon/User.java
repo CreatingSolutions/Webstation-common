f3763535603c7a1c5e7ffeb98cf70658303dca3e
SAUVALLE Damien <damien.sauvalle.etu@univ-lille.fr>
2019-01-21 17:43:05 +0100

    feat : update user get/set

--- src/main/java/webstationcommon/User.java
+++ src/main/java/webstationcommon/User.java
@@ -10,4 +10,36 @@ public class User extends Entity {
 
     private Cart cart;
 
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Token getToken() {
+        return token;
+    }
+
+    public void setToken(Token token) {
+        this.token = token;
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
 }
